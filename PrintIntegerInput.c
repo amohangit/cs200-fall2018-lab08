8b845819e9fcba7cce31b4f875e33a50cf986b5d
Aravind Mohan <amohan@ewire23-47.allegheny.edu>
2018-09-07 11:54:27 -0400

    first commit

---
+++ PrintIntegerInput.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int number;
+
+    // printf() displays the formatted output 
+    printf("Enter an integer: ");  
+    
+    // scanf() reads the formatted input and stores them
+    scanf("%d", &number);  
+    
+    // printf() displays the formatted output
+    printf("You entered: %d", number);
+    return 0;
+
+} //main
